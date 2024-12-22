fd9177016117bdb9334fe5853cf9e9470d9aa435
phkhang <trannguyenphuckhang12@gmail.com>
2024-12-23 00:23:48 +0630

    Update: make client jar

--- mecha-client/src/main/java/com/example/mechaclient/ChatApplication.java
+++ mecha-client/src/main/java/com/example/mechaclient/ChatApplication.java
@@ -12,7 +12,7 @@ import com.example.mechaclient.models.UserSession;
 public class ChatApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ChatApplication.class.getResource("views/LoginScreen.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(ChatApplication.class.getResource("/views/LoginScreen.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("User Chat Application");
         stage.setScene(scene);
---
+++ mecha-client/src/main/java/com/example/mechaclient/Main.java
@@ -0,0 +1,7 @@
+package com.example.mechaclient;
+
+public class Main {
+    public static void main(String[] args) {
+        ChatApplication.main(args);
+    }
+}
